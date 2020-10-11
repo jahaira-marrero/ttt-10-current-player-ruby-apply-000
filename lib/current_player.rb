095748cd7fc444153512e244da5dbf3d2690fdc9
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-10-11 15:25:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,8 @@ def turn_count(board)
   turn_counter = 0 
   
   board.each do |character|
-  if character != " " && character != ""
+  if character != " " && 
+    character != ""
     turn_counter += 1
   end
 end
